a766401a1de3330f499981a653c5bc8f24dc5170
gergarcia <ggarcia@openmailbox.org>
2015-07-24 15:48:47 -0300

    Update and rename main.css to main.js
    Codecademy: Animate your name

---
+++ main.js
@@ -0,0 +1,18 @@
+var red = [230, 30, 30];
+var orange = [241, 120, 91];
+var green = [7, 100, 13];
+var blue = [49, 95, 122];
+var purple = [33, 63, 82];
+
+var myName = "www.COPIAPEL.com";
+letterColors = [red]
+if(10 > 3) {    
+bubbleShape = "circle";
+}
+
+else {
+    bubbleShape = "square";
+}
+drawName(myName, letterColors);
+bounceName();
+bounceBubbles();
